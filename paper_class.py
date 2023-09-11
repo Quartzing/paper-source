37782168176f288ca493ce464a87210550a7bdcc
takahirosir <98999068+takahirosir@users.noreply.github.com>
2023-09-11 12:04:14 +0800

    Update paper_class.py with arxiv API

--- paper_class.py
+++ paper_class.py
@@ -5,6 +5,7 @@ from tools import download_link
 import requests
 import feedparser
 import subprocess
+import arxiv
 
 
 class Paper(object):
@@ -89,48 +90,65 @@ class Paper(object):
         """
         return f'{self.authors[0]} et al. ({self.publish_date.tm_year})'
 
-def get_papers(query: str, max_results: int, download: bool = False, sort_type: str = "relevance",
-               sort_order: str = "descending") -> Dict[str, Paper]:
+def get_papers(search_results,
+               download: bool = False) -> Dict[str, Paper]:
     """
     Search for papers on arXiv and optionally download them.
 
     Args:
-        query (str): The search query.
+        query (str): The search query. Use example `au:XXX AND ti:XXXX` 
         max_results (int): The maximum number of results to retrieve.
         download (bool, optional): Whether to download the papers. Defaults to False.
-        sort_type (str, optional): The sorting type for the search results. Defaults to "relevance".
-        sort_order (str, optional): The sorting order for the search results. Defaults to "descending".
+        sort_type (str, optional): The sorting type for the search results. Defaults to "SubmittedDate".
+        sort_order (str, optional): The sorting order for the search results. Defaults to "Descending".
 
     Returns:
         Dict[str, Paper]: A dictionary of Paper objects with titles as keys.
     """
     print(f"Searching {query} from arxiv... (max #results: {max_results})")
     # Perform the arXiv search using the arXiv API
-    base_url: str = "http://export.arxiv.org/api/query?"
-    search_query: str = f"search_query=all:{query}&start=0&max_results={max_results}&sortBy={sort_type}&sortOrder={sort_order}"
+    # base_url: str = "http://export.arxiv.org/api/query?"
+    # search_query: str = f"search_query=all:{query}&start=0&max_results={max_results}&sortBy={sort_type}&sortOrder={sort_order}"
 
-    response: requests.Response = requests.get(base_url + search_query)
-    feed: feedparser.FeedParserDict = feedparser.parse(response.text)
-
-    # Create a directory to store the downloaded papers
-    output_directory: str = "arxiv_papers"
-    os.makedirs(output_directory, exist_ok=True)
+    # response: requests.Response = requests.get(base_url + search_query)
+    # feed: feedparser.FeedParserDict = feedparser.parse(response.text)
 
     results: Dict[str, Paper] = {}
-    # Loop through the search results and download papers
-    for entry in feed.entries:
-        paper_link: str = [link for link in entry.links if link.type == "application/pdf"][0].href
+    for result in search_results():
+        paper_link: result.entry_id
         paper_link += '.pdf'
-        paper: Paper = Paper(title=entry.title,
+        paper: Paper = Paper(title=result.title,
                       summary=entry.summary,
                       url=paper_link,
-                      authors=[author['name'] for author in entry.authors],
-                      publish_date=entry.published_parsed)
+                      authors=result.author
+                      publish_date=result.published)
         results[paper.title] = paper
         if download:
             paper.download(use_title=True)
 
     return results
+        
+                   
+    # Create a directory to store the downloaded papers
+    output_directory: str = "arxiv_papers"
+    os.makedirs(output_directory, exist_ok=True)
+
+    # results: Dict[str, Paper] = {}
+    # Loop through the search results and download papers
+                   
+    # for entry in feed.entries:
+    #     paper_link: str = [link for link in entry.links if link.type == "application/pdf"][0].href
+    #     paper_link += '.pdf'
+    #     paper: Paper = Paper(title=entry.title,
+    #                   summary=entry.summary,
+    #                   url=paper_link,
+    #                   authors=[author['name'] for author in entry.authors],
+    #                   publish_date=entry.published_parsed)
+    #     results[paper.title] = paper
+    #     if download:
+    #         paper.download(use_title=True)
+
+    # return results
 
 if __name__ == '__main__':
     results: Dict[str, Paper] = get_papers(
