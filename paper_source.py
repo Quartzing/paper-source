c880664c3c6b44329f9dbfb9157375de0941fe7c
Quartzing <Quartzing@users.noreply.github.com>
2023-09-22 15:14:53 +0000

    Revert "Split PaperSource and PaperContentSource classes (#12)"
    This reverts commit 83d69746de8f11718fad1a2b4d3058f14b122a3c.

--- paper_source.py
+++ paper_source.py
@@ -1,6 +1,5 @@
 import openai
 import os
-from abc import ABC, abstractmethod
 from typing import Dict, List
 import uuid
 from langchain.docstore.document import Document
@@ -13,17 +12,20 @@ from tools import *
 from paper_class import Paper
 
 
-class PaperSource(ABC):
+class PaperSource:
     def __init__(self, 
                  papers: Dict[str, Paper], 
-                 openai_api_key: str):
+                 openai_api_key: str,
+                 ignore_references: bool = True):
         """
         Initializes a PaperSource object with a dictionary of papers and an OpenAI API key.
 
         Args:
             papers (Dict[str, Paper]): A dictionary containing paper titles as keys and object of class Paper as values.
             openai_api_key (str): The OpenAI API key for text embeddings.
+            ignore_references (bool): Whether to ignore the chunks containing references.
         """
+        self.ignore_references_ = ignore_references
         self.papers_: Dict[str, Paper] = papers
         doc_list: List[Document] = []
         for title, paper in papers.items():
@@ -64,56 +66,6 @@ class PaperSource(ABC):
         """
         return self.papers_
 
-    @abstractmethod
-    def _process_paper(self, paper: Paper) -> List[Document]:
-        """
-        Process a paper.
-
-        Args:
-            paper (Paper): A Paper object representing the paper to be processed.
-
-        Returns:
-            List[Document]: A list of Document objects, each containing a text chunk with metadata.
-        """
-        raise NotImplementedError(f"Please don not use this abstract class.")
-
-    def retrieve(self, query: str, num_retrieval: int | None =None) -> List[Document]:
-        """
-        Search for papers related to a query using text embeddings and cosine distance.
-
-        Args:
-            query (str): The query string to search for related papers.
-
-        Returns:
-            List[Document]: A list of Document objects representing the related papers found.
-        """
-        print(f'Searching for related works of: {query}...')
-        if not num_retrieval:
-            num_retrieval = len(self.papers_)
-        sources: List[Document] = self.db_.similarity_search(query, k=num_retrieval)
-        print(f'{len(sources)} sources found.')
-        return sources
-
-
-class PaperContentSource(PaperSource):
-    def __init__(self, 
-                 papers: Dict[str, Paper], 
-                 openai_api_key: str,
-                 ignore_references: bool = True):
-        """
-        Initializes a PaperSource object with a dictionary of papers and an OpenAI API key.
-
-        Args:
-            papers (Dict[str, Paper]): A dictionary containing paper titles as keys and object of class Paper as values.
-            openai_api_key (str): The OpenAI API key for text embeddings.
-            ignore_references (bool): Whether to ignore the chunks containing references.
-        """
-        self.ignore_references_ = ignore_references
-        super().__init__(
-            papers=papers,
-            openai_api_key=openai_api_key,
-        )
-
     def _process_paper(self, paper: Paper) -> List[Document]:
         """
         Process a paper.
@@ -151,11 +103,28 @@ class PaperContentSource(PaperSource):
         
         return doc_list
 
+    def retrieve(self, query: str, num_retrieval: int | None =None) -> List[Document]:
+        """
+        Search for papers related to a query using text embeddings and cosine distance.
+
+        Args:
+            query (str): The query string to search for related papers.
+
+        Returns:
+            List[Document]: A list of Document objects representing the related papers found.
+        """
+        print(f'Searching for related works of: {query}...')
+        if not num_retrieval:
+            num_retrieval = len(self.papers_)
+        sources: List[Document] = self.db_.similarity_search(query, k=num_retrieval)
+        print(f'{len(sources)} sources found.')
+        return sources
+
 
 if __name__ == '__main__':
     from test_utils import get_test_papers
 
-    paper_source = PaperContentSource(
+    paper_source = PaperSource(
         papers=get_test_papers(),
         openai_api_key='',
         ignore_references=True,
