434387ea29371dcef152db6495986f9a51c7fb3a
Quartzing <Quartzing@users.noreply.github.com>
2023-09-22 20:19:31 +0800

    Add test utils (#10)

---
+++ test_utils.py
@@ -0,0 +1,23 @@
+from paper_class import Paper
+from datetime import datetime
+
+
+def get_test_papers():
+    papers = {
+        "paper 1": Paper(
+            title="paper 1",
+            summary="",
+            authors=[],
+            url="https://arxiv.org/pdf/2309.00240",
+            publish_date=datetime.strptime("2020", "%Y"),
+        ),
+        "paper 2": Paper(
+            title="paper 2",
+            summary="",
+            authors=[],
+            url="https://arxiv.org/pdf/2309.00087",
+            publish_date=datetime.strptime("2020", "%Y"),
+        ),
+    }
+
+    return papers
